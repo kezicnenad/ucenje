30095a060ab6a8e45e18b262eacb664989afea28
Kežić Nenad <gluvaja7384@gmail.com>
2023-03-13 06:13:57 +0100

    Deploy

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 
-const port = process.env.PORT || 5000
-
-dotenv.config()
+const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
   res.send('Apstinencijska Kviza REST API Server running')
 })
 
-// SETUP SERVER
 app.listen(port, () => {
-  console.log("Server is running on port ", port)
+  console.log(`Server is running on port ${port}`)
 })
